70bac690be212a8254996a20c6f3770e6245c387
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2016-10-10 14:18:02 +0300

    Make minor changes to the screen "Search"

--- app/src/main/java/com/akhbulatov/wordkeeper/ui/SearchActivity.java
+++ app/src/main/java/com/akhbulatov/wordkeeper/ui/SearchActivity.java
@@ -17,9 +17,7 @@
 package com.akhbulatov.wordkeeper.ui;
 
 import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -29,44 +27,45 @@ import android.widget.TextView;
 
 import com.akhbulatov.wordkeeper.R;
 import com.akhbulatov.wordkeeper.adapter.WordAdapter;
-import com.akhbulatov.wordkeeper.database.DatabaseAdapter;
+import com.akhbulatov.wordkeeper.database.DatabaseWordAdapter;
 import com.akhbulatov.wordkeeper.ui.widget.DividerItemDecoration;
 
 /**
- * Displays the search results words
+ * Shows search results words
  */
-public class SearchableActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searchable);
+        setContentView(R.layout.activity_search);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
-        Intent intent = getIntent();
         // Receives a query from ACTION_SEARCH
-        String query = intent.getStringExtra(SearchManager.QUERY);
-
-        DatabaseAdapter dbAdapter = new DatabaseAdapter(this);
-        dbAdapter.open();
+        String query = getIntent().getStringExtra(SearchManager.QUERY);
 
         RecyclerView wordList = (RecyclerView) findViewById(R.id.recycler_word_list);
         wordList.setHasFixedSize(true);
-        wordList.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        wordList.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL_LIST));
         wordList.setLayoutManager(new LinearLayoutManager(this));
 
-        WordAdapter wordAdapter = new WordAdapter(dbAdapter.fetchRecordsByName(query));
+        DatabaseWordAdapter dbWordAdapter = new DatabaseWordAdapter(this);
+        dbWordAdapter.open();
+
+        WordAdapter wordAdapter = new WordAdapter(dbWordAdapter.fetchRecordsByName(query));
         wordAdapter.setHasStableIds(true);
         wordList.setAdapter(wordAdapter);
 
         TextView textNoSearchResults = (TextView) findViewById(R.id.text_no_search_results);
 
         if (wordAdapter.getItemCount() == 0) {
-            String noSearchResults = String.format(getString(R.string.msg_no_search_results), query);
+            String noSearchResults = String.format(getString(R.string.no_search_results), query);
 
             textNoSearchResults.setText(noSearchResults);
             textNoSearchResults.setVisibility(View.VISIBLE);
@@ -74,8 +73,7 @@ public class SearchableActivity extends AppCompatActivity {
             textNoSearchResults.setVisibility(View.INVISIBLE);
         }
 
-        // Closes the database already here.
-        // After the search results display access to the database is no longer needed
-        dbAdapter.close();
+        // Closes the database already here, as it is no longer needed
+        dbWordAdapter.close();
     }
 }
