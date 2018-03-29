5bf0af5b61b91bf62f65174a071c6f015683fd9f
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2018-03-29 21:38:47 +0300

    Update year of copyright

--- app/src/main/java/com/akhbulatov/wordkeeper/event/CategoryEditEvent.java
+++ app/src/main/java/com/akhbulatov/wordkeeper/event/CategoryEditEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Alidibir Akhbulatov
+ * Copyright 2018 Alidibir Akhbulatov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
