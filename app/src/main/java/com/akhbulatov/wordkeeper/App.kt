f72e7325dd39081caad53d0f51d81e5d21d8ebbc
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2020-05-29 16:05:35 +0300

    Remove Fabric Crashlytics

--- app/src/main/java/com/akhbulatov/wordkeeper/App.kt
+++ app/src/main/java/com/akhbulatov/wordkeeper/App.kt
@@ -3,10 +3,7 @@ package com.akhbulatov.wordkeeper
 import android.app.Application
 import com.akhbulatov.wordkeeper.di.AppComponent
 import com.akhbulatov.wordkeeper.di.DaggerAppComponent
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import com.facebook.stetho.Stetho
-import io.fabric.sdk.android.Fabric
 
 class App : Application() {
 
@@ -14,7 +11,6 @@ class App : Application() {
         super.onCreate()
         initDI()
         initStetho()
-        initCrashlytics()
     }
 
     private fun initDI() {
@@ -25,17 +21,6 @@ class App : Application() {
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
         }
-    }
-
-    private fun initCrashlytics() {
-        val crashlyticsKit = Crashlytics.Builder()
-            .core(
-                CrashlyticsCore.Builder()
-                    .disabled(BuildConfig.DEBUG)
-                    .build()
-            )
-            .build()
-        Fabric.with(this, crashlyticsKit)
     }
 
     companion object {
